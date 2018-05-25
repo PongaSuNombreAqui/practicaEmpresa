b102299a8dfb4fbc8f26e75fa605221f39f2dfa8
Abel Hermoso Espada <abelhermosoespada@gmail.com>
2018-05-25 23:52:30 +0200

    var numero, parametros
    el numero pedido se genera fuera para simplificar en el UI
    la fecha no entra como parametro
    el cliente tampoco, sino su key

--- src/control/AccionesPedido.java
+++ src/control/AccionesPedido.java
@@ -4,41 +4,61 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 import modelo.Cliente;
+import modelo.DAO;
 import modelo.LineaPedido;
 import modelo.Pedido;
+import utiles.Utiles;
 import modelo.Articulo;
 import javax.swing.JLabel;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
 public class AccionesPedido {
 
-	public void crear(ArrayList<LineaPedido> lineaPedido, Cliente cliente, LocalDate fecha) {
-		Pedido pedido = new Pedido(cliente, lineaPedido, fecha);
-		pedido = new Pedido(cliente, lineaPedido, fecha);
+	public boolean crear(ArrayList<LineaPedido> lineaPedido, int numeroCliente) {
+		LocalDate fecha = null;//TODO hay que poner que entre la fecha aqui
+		Cliente cliente = (Cliente) new AlmacenCliente<>().obtener(numeroCliente);
+		int numero=getNumeroPosiblePedido();
+		Pedido pedido = new Pedido(cliente, lineaPedido, fecha,numero);
 		AlmacenPedido almacen = new AlmacenPedido();
-
-		almacen.grabar(cliente.getID(), pedido);
+		if(almacen.grabar(cliente.getID(), pedido)){
+			aumentarNumeroPedido(numero);
+			return true;
+		}
+		return false;
+		
+	}
+	
+	private void aumentarNumeroPedido(int numero) {
+		new DAO<>().grabar(Utiles.pathNumerosPedido,numero+1);
 	}
 
+	public int getNumeroPosiblePedido(){
+		int leer = 0;
+		if(Utiles.comprobarExiste(Utiles.pathNumerosPedido)){
+			leer = (int) new DAO<>().leer(Utiles.pathNumerosPedido); 
+		}
+		return leer;
+	}
+	
 	public void consultar(String id, int numeroPedido) {
 		AlmacenPedido almacen = new AlmacenPedido();
 		Pedido pedido = almacen.leer(id, numeroPedido);
 		// ?
 
 	}
+	
 
+	public void aniadirArticuloATabla(JTable tabla, String nombre) {
+		DefaultTableModel dm = (DefaultTableModel) tabla.getModel();
+		Articulo item = new AlmacenArticulo<Articulo>().leer(nombre);
+		dm.addRow(introducirRejilla(item));
+	}
 	private Object[] introducirRejilla(Articulo item) {
 		int cantidadAlAniadirse = 1;
 		Object[] retorno = { item.getId(), item.getNombre(), item.getPrecio(), cantidadAlAniadirse,
 				item.getPrecio() * cantidadAlAniadirse };
 		return retorno;
-	}
-
-	public void añadirArticuloATabla(JTable tabla, String nombre) {
-		DefaultTableModel dm = (DefaultTableModel) tabla.getModel();
-		Articulo item = new AlmacenArticulo<Articulo>().leer(nombre);
-		dm.addRow(introducirRejilla(item));
-
 	}
 }
