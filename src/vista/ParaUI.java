38ed97de871392e6ae5247344e59a6baeab91778
Vladek <frandr96@gmail.com>
2018-06-06 09:59:38 +0200

    modificacion del listener editar

--- src/vista/ParaUI.java
+++ src/vista/ParaUI.java
@@ -63,6 +63,13 @@ public class ParaUI extends UI {
 	 * listeners que usa el panel de articulos
 	 */
 	private void ponerListenerArticulo() {
+		panelEditarArticulo.getNuevoPrecio().addKeyListener(new KeyAdapter() {
+			public void keyTyped(KeyEvent e) {
+				if ((e.getKeyChar() < '0' && e.getKeyChar() != '.' || e.getKeyChar() > '9') && e.getKeyChar() != '.') {
+					e.consume();
+				}
+			}
+		});
 
 		panelArticulo.getCrearPrecio().addKeyListener(new KeyAdapter() {
 			public void keyTyped(KeyEvent e) {
@@ -148,27 +155,20 @@ public class ParaUI extends UI {
 					}
 				}
 			}
-
-			private boolean comprobarPuntos(String text) {
-				for (int i = 0, contador = 0; i < text.length(); i++) {
-					if (text.charAt(i) == '.') {
-						contador++;
-						if (contador > 1) {
-							return true;
-						}
-					}
-				}
-				return false;
-			}
 		});
 
 		panelEditarArticulo.getBtnEditar().addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (!panelArticulo.getDetallesNombre().getText().isEmpty()) {
-					logica.editar(panelArticulo.getDetallesNombre().getText(),
-							Float.valueOf(panelEditarArticulo.getNuevoPrecio().getText()));
-					panelArticulo.getDetallesPrecio().setText(panelEditarArticulo.getNuevoPrecio().getText());
-					panelEditarArticulo.getNuevoPrecio().setText("");
+				if (false == panelEditarArticulo.getNuevoPrecio().getText().isEmpty()) {
+					if (false == comprobarPuntos(panelEditarArticulo.getNuevoPrecio().getText())) {
+						logica.editar(panelArticulo.getDetallesNombre().getText(),
+								Float.valueOf(panelEditarArticulo.getNuevoPrecio().getText()));
+						panelArticulo.getDetallesPrecio().setText(panelEditarArticulo.getNuevoPrecio().getText());
+						panelEditarArticulo.getNuevoPrecio().setText("");
+					} else {
+						setMensaje("Error: El nuevo precio esta mal escrito!!", Color.RED,
+								panelArticulo.getTextMensajeSistema());
+					}
 				} else {
 					setMensaje("Error: Vacio!!", Color.RED, panelArticulo.getTextMensajeSistema());
 				}
@@ -200,7 +200,6 @@ public class ParaUI extends UI {
 	private void ponerListenerCliente() {
 		panelCliente.getBtnAgregar().addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-
 				if (!comprobarCamposTxt(panelCliente.getTxtDnicif(), panelCliente.getTxtRazonSocial(),
 						panelCliente.getTxtDireccion(), panelCliente.getTxtTelefono())) {
 					if (Validator.isDniCif(panelCliente.getTxtDnicif().getText())) {
@@ -270,7 +269,6 @@ public class ParaUI extends UI {
 
 		panelCliente.getBtnEliminarCliente().addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-
 				int resultado = JOptionPane.showConfirmDialog(null, "¿Seguro que deseas eliminar este cliente?", "",
 						JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null);
 				if (resultado == JOptionPane.YES_OPTION) {
@@ -651,4 +649,15 @@ public class ParaUI extends UI {
 		return vacio;
 	}
 
+	private boolean comprobarPuntos(String text) {
+		for (int i = 0, contador = 0; i < text.length(); i++) {
+			if (text.charAt(i) == '.') {
+				contador++;
+				if (contador > 1) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
